2de9206c11dd638fd6d75a79326f03688435df50
loles.traore@gmail.com <loles.traore@gmail.com>
2019-03-20 11:00:38 +0100

    add contact logic

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { DomSanitizer, SafeUrl} from '@angular/platform-browser';
 import { Contact } from '../../models/Contact';
+import { NgForm } from '@angular/forms';
+import { ContactService } from 'src/app/services/contact.service';
 
 @Component({
   selector: 'app-contact',
@@ -12,14 +14,32 @@ export class ContactComponent implements OnInit {
 
   @Input() contact: Contact;
   public safeUrl: SafeUrl;
+  public sendError: boolean;
+  public submitted: boolean;
 
   constructor(
+    private contactService: ContactService,
     private domSanitizer: DomSanitizer
   ) { }
 
 
   ngOnInit() {
     this.safeUrl = this.domSanitizer.bypassSecurityTrustResourceUrl(this.contact.mapUrl);
+    this.sendError = false;
+    this.submitted = false;
+  }
+
+  sendMessage(form: NgForm) {
+    this.sendError = false;
+
+    const name = form.value.name;
+    const email = form.value.email;
+    const message = form.value.message;
+
+    this.contactService.postMessage(name, email, message, this.contact.mail ).then(
+      success => this.submitted = true,
+      error => this.sendError = true
+    );
   }
 
 }
